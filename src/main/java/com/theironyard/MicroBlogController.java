1b52ec58131ec8bd65b9a67fa94ad2060e67f6a9
BennettIronYard <bennettiy12@gmail.com>
2015-11-09 14:21:51 -0500

    initial push. delete/message

---
+++ src/main/java/com/theironyard/MicroBlogController.java
@@ -0,0 +1,41 @@
+package com.theironyard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
+/**
+ * Created by BennettIronYard on 11/9/15.
+ */
+@Controller
+public class MicroBlogController {
+    ArrayList<Message> messages = new ArrayList(); //Create an ArrayList<Message> in your controller to store submitted messages
+
+    @RequestMapping("/")
+    public String home(Model model, HttpServletRequest request) { //It should take the model and the request as arguments
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username"); //It should read the username from the session and add it to the model
+        model.addAttribute("username", username);
+        return "home"; //It should return the home template
+    }
+    @RequestMapping("login")
+    public String login(HttpServletRequest request, String username) { //It should take the request and the username as arguments
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username); //It should save the username to the session
+        return "redirect:/"; //It should return a redirect to /
+    }
+
+    @RequestMapping("add-message")
+    public String submittedMessages(String text) { //It should take the message text as an argument
+
+        Message message = new Message(messages.size() + 1, text);
+        messages.add(message); //It should create a Message object and add it to the arraylist
+        return "redirect:/"; //It should return a redirect to /
+
+    }
+
+}
