ae42e62debcf07a8a42fdebc5bad653239c81913
BennettIronYard <bennettiy12@gmail.com>
2015-11-09 16:03:23 -0500

    Done with all functions

--- src/main/java/com/theironyard/MicroBlogController.java
+++ src/main/java/com/theironyard/MicroBlogController.java
@@ -20,22 +20,31 @@ public class MicroBlogController {
         HttpSession session = request.getSession();
         String username = (String) session.getAttribute("username"); //It should read the username from the session and add it to the model
         model.addAttribute("username", username);
+        model.addAttribute("messages", messages);
         return "home"; //It should return the home template
     }
-    @RequestMapping("login")
+    @RequestMapping("/login")
     public String login(HttpServletRequest request, String username) { //It should take the request and the username as arguments
         HttpSession session = request.getSession();
         session.setAttribute("username", username); //It should save the username to the session
         return "redirect:/"; //It should return a redirect to /
     }
 
-    @RequestMapping("add-message")
+    @RequestMapping("/add-message")
     public String submittedMessages(String text) { //It should take the message text as an argument
-
         Message message = new Message(messages.size() + 1, text);
         messages.add(message); //It should create a Message object and add it to the arraylist
         return "redirect:/"; //It should return a redirect to /
-
+    }
+    @RequestMapping("/delete-message")
+    public String id(Integer id) {  //It should take the message id as an argument (the type should be Integer)
+        messages.remove(id - 1);
+        int i = 1;
+        for (Message message : messages) {
+            message.id = i;
+            i++;
+        }
+        return "redirect:/";     //It should return a redirect to
     }
 
 }
